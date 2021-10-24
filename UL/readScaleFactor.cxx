2ae0ea21e9be231ab40d89639e0ff12ee141f4b6
Christopher Matthies <Christopher.Matthies@desy.de>
2021-10-24 19:25:33 +0200

    more ROOT-ish way of naming things & close TFile at end of loadScaleFactor function

--- UL/readScaleFactor.cxx
+++ UL/readScaleFactor.cxx
@@ -7,24 +7,24 @@
 //______________________________
 // CONSTANTS AND TYPEDEFS
 
-enum class Year {
+enum class EYear {
   isUL17,
   isUL18,
 };
 
-const std::map<Year, TString> rootFiles = {
-  {Year::isUL17, "scaleFactors/TopTaggingScaleFactors_RunIISummer19UL17_PUPPIv15.root"},
-  {Year::isUL18, "scaleFactors/TopTaggingScaleFactors_RunIISummer19UL18_PUPPIv15.root"},
+const std::map<EYear, TString> kRootFiles = {
+  {EYear::isUL17, "scaleFactors/TopTaggingScaleFactors_RunIISummer19UL17_PUPPIv15.root"},
+  {EYear::isUL18, "scaleFactors/TopTaggingScaleFactors_RunIISummer19UL18_PUPPIv15.root"},
 };
 
-enum class Tagger {
+enum class ETagger {
   isAK8,
   isAK8_subjetbtag,
   isHOTVR,
 };
 
-enum class WorkingPoint {
-  isWP0p38, // for AK8 taggers
+enum class EWorkingPoint {
+  isWP0p38, // for AK8 ETaggers
   isWP0p47, // %
   isWP0p52, // %
   isWP0p61, // %
@@ -32,51 +32,51 @@ enum class WorkingPoint {
   isDefault, // for HOTVR only
 };
 
-enum class MergeCategory {
+enum class EMergeCategory {
   isFullyMerged,
   isNotMerged,
 };
 
-const std::map<Tagger, TString> tagger_strings = {
-  {Tagger::isAK8, "AK8_PUPPI"},
-  {Tagger::isAK8_subjetbtag, "AK8_PUPPI_subjetbtag"},
-  {Tagger::isHOTVR, "HOTVR_PUPPI"},
+const std::map<ETagger, TString> kTaggerToString = {
+  {ETagger::isAK8, "AK8_PUPPI"},
+  {ETagger::isAK8_subjetbtag, "AK8_PUPPI_subjetbtag"},
+  {ETagger::isHOTVR, "HOTVR_PUPPI"},
 };
 
-const std::map<WorkingPoint, TString> wp_strings = {
-  {WorkingPoint::isWP0p38, "wp0p38"},
-  {WorkingPoint::isWP0p47, "wp0p47"},
-  {WorkingPoint::isWP0p52, "wp0p52"},
-  {WorkingPoint::isWP0p61, "wp0p61"},
-  {WorkingPoint::isWP0p69, "wp0p69"},
-  {WorkingPoint::isDefault, "default"},
+const std::map<EWorkingPoint, TString> kWorkingPointToString = {
+  {EWorkingPoint::isWP0p38, "wp0p38"},
+  {EWorkingPoint::isWP0p47, "wp0p47"},
+  {EWorkingPoint::isWP0p52, "wp0p52"},
+  {EWorkingPoint::isWP0p61, "wp0p61"},
+  {EWorkingPoint::isWP0p69, "wp0p69"},
+  {EWorkingPoint::isDefault, "default"},
 };
 
-const std::map<MergeCategory, TString> mergecat_strings = {
-  {MergeCategory::isFullyMerged, "FullyMerged"},
-  {MergeCategory::isNotMerged, "NotMerged"},
+const std::map<EMergeCategory, TString> kMergeCategoryToString = {
+  {EMergeCategory::isFullyMerged, "FullyMerged"},
+  {EMergeCategory::isNotMerged, "NotMerged"},
 };
 
 typedef struct {
-  Double_t nominal = 1.; // central value
-  Double_t err_tot_up = 0.; // total uncertainty
-  Double_t err_tot_down = 0.; // %
-  Double_t err_stat_up = 0.; // statistical component
-  Double_t err_stat_down = 0.; // %
-  Double_t err_syst_up = 0.; // systematic component
-  Double_t err_syst_down = 0.; // %
-} ScaleFactor;
+  Double_t fCentral; // central value
+  Double_t fErrTotUp; // total uncertainty
+  Double_t fErrTotDown; // %
+  Double_t fErrStatUp; // statistical component
+  Double_t fErrStatDown; // %
+  Double_t fErrSystUp; // systematic component
+  Double_t fErrSystDown; // %
+} ScaleFactor_t;
 
 
 //______________________________
 // FUNCTIONS
 
-ScaleFactor loadScaleFactor(const Double_t jetPt, const Tagger tagger, const WorkingPoint wp, const MergeCategory mergeCat, const Year year) {
-  if((wp == WorkingPoint::isDefault && tagger != Tagger::isHOTVR) || (tagger == Tagger::isHOTVR && wp != WorkingPoint::isDefault)) {
+ScaleFactor_t loadScaleFactor(const Double_t jetPt, const ETagger tagger, const EWorkingPoint wp, const EMergeCategory mergeCat, const EYear year) {
+  if((wp == EWorkingPoint::isDefault && tagger != ETagger::isHOTVR) || (tagger == ETagger::isHOTVR && wp != EWorkingPoint::isDefault)) {
     throw std::invalid_argument("Given combination of tagger/WP not valid");
   }
-  TFile *rootFile = TFile::Open(rootFiles.at(year).Data(), "READ");
-  TString graph_basename = tagger_strings.at(tagger)+"_"+wp_strings.at(wp)+"/"+mergecat_strings.at(mergeCat)+"_";
+  TFile *rootFile = TFile::Open(kRootFiles.at(year).Data(), "READ");
+  const TString graph_basename = kTaggerToString.at(tagger)+"_"+kWorkingPointToString.at(wp)+"/"+kMergeCategoryToString.at(mergeCat)+"_";
   const TGraphAsymmErrors *graph_tot = (TGraphAsymmErrors*)rootFile->Get((graph_basename+"tot").Data());
   const TGraphAsymmErrors *graph_stat = (TGraphAsymmErrors*)rootFile->Get((graph_basename+"stat").Data());
   const TGraphAsymmErrors *graph_syst = (TGraphAsymmErrors*)rootFile->Get((graph_basename+"syst").Data());
@@ -86,7 +86,7 @@ ScaleFactor loadScaleFactor(const Double_t jetPt, const Tagger tagger, const Wor
   Double_t y;
   graph_tot->GetPoint(ibin, x, y);
 
-  Double_t minimum_jetPt = x - graph_tot->GetErrorXlow(ibin);
+  const Double_t minimum_jetPt = x - graph_tot->GetErrorXlow(ibin);
   if(jetPt < minimum_jetPt) {
     throw std::out_of_range("No scale factor for such low jet pT available");
   }
@@ -97,34 +97,37 @@ ScaleFactor loadScaleFactor(const Double_t jetPt, const Tagger tagger, const Wor
     if(ibin + 1 >= graph_tot->GetN()) break; // use scale factor of last bin if given jetPt exceeds highest bin edge of the graph
   }
 
-  ScaleFactor sf;
-  sf.nominal = y;
-  sf.err_tot_up = graph_tot->GetErrorYhigh(ibin);
-  sf.err_tot_down = graph_tot->GetErrorYlow(ibin);
-  sf.err_stat_up = graph_stat->GetErrorYhigh(ibin);
-  sf.err_stat_down = graph_stat->GetErrorYlow(ibin);
-  sf.err_syst_up = graph_syst->GetErrorYhigh(ibin);
-  sf.err_syst_down = graph_syst->GetErrorYlow(ibin);
+  const ScaleFactor_t sf = ScaleFactor_t{
+    .fCentral = y,
+    .fErrTotUp = graph_tot->GetErrorYhigh(ibin),
+    .fErrTotDown = graph_tot->GetErrorYlow(ibin),
+    .fErrStatUp = graph_stat->GetErrorYhigh(ibin),
+    .fErrStatDown = graph_stat->GetErrorYlow(ibin),
+    .fErrSystUp = graph_syst->GetErrorYhigh(ibin),
+    .fErrSystDown = graph_syst->GetErrorYlow(ibin)
+  };
+
+  rootFile->Close();
 
   return sf;
 }
 
-void printScaleFactor(const ScaleFactor & sf, const Int_t precision = 3) {
+void printScaleFactor(const ScaleFactor_t & sf, const Int_t precision = 3) {
   std::stringstream ss;
   ss << "SF: ";
-  ss << std::fixed << std::setprecision(precision) << sf.nominal;
-  ss << " +" << std::fixed << std::setprecision(precision) << sf.err_tot_up;
+  ss << std::fixed << std::setprecision(precision) << sf.fCentral;
+  ss << " +" << std::fixed << std::setprecision(precision) << sf.fErrTotUp;
   ss << "/";
-  ss << "-" << std::fixed << std::setprecision(precision) << sf.err_tot_down;
+  ss << "-" << std::fixed << std::setprecision(precision) << sf.fErrTotDown;
   ss << " (tot)";
   ss << " [";
-  ss << " +" << std::fixed << std::setprecision(precision) << sf.err_stat_up;
+  ss << " +" << std::fixed << std::setprecision(precision) << sf.fErrStatUp;
   ss << "/";
-  ss << "-" << std::fixed << std::setprecision(precision) << sf.err_stat_down;
+  ss << "-" << std::fixed << std::setprecision(precision) << sf.fErrStatDown;
   ss << " (stat) |";
-  ss << " +" << std::fixed << std::setprecision(precision) << sf.err_syst_up;
+  ss << " +" << std::fixed << std::setprecision(precision) << sf.fErrSystUp;
   ss << "/";
-  ss << "-" << std::fixed << std::setprecision(precision) << sf.err_syst_down;
+  ss << "-" << std::fixed << std::setprecision(precision) << sf.fErrSystDown;
   ss << " (syst) ]";
   std::cout << ss.str() << std::endl;
 }
@@ -135,12 +138,12 @@ void printScaleFactor(const ScaleFactor & sf, const Int_t precision = 3) {
 
 void readScaleFactor() {
   // Exemplary configuration:
-  Double_t jetPt = 345.6789; // in GeV
-  Tagger tagger = Tagger::isAK8;
-  WorkingPoint wp = WorkingPoint::isWP0p52;
-  MergeCategory mergeCat = MergeCategory::isFullyMerged; // in practice, this needs to be determined for each individual jet; see TWiki how to do it
-  Year year = Year::isUL17;
+  const Double_t jetPt = 345.6789; // in GeV
+  const ETagger tagger = ETagger::isAK8;
+  const EWorkingPoint wp = EWorkingPoint::isWP0p52;
+  const EMergeCategory mergeCat = EMergeCategory::isFullyMerged; // in practice, this needs to be determined for each individual jet; see TWiki how to do it
+  const EYear year = EYear::isUL17;
 
-  ScaleFactor sf = loadScaleFactor(jetPt, tagger, wp, mergeCat, year);
+  const ScaleFactor_t sf = loadScaleFactor(jetPt, tagger, wp, mergeCat, year);
   printScaleFactor(sf);
 }
